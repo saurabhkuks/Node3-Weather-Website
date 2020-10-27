f85ba38390f231ec302667a0fdf3729efd8faef8
Saurabh Kukreti <saurabhss11c@gmail.com>
2020-10-27 23:39:23 +0530

    Removed unnecessary console.log command

--- public/JS/app.js
+++ public/JS/app.js
@@ -1,5 +1,5 @@
-console.log('client side js file')
-
+/*console.log('client side js file')
+*/
 fetch('http://puzzle.mead.io/puzzle').then((response)=>{
     response.json().then((data)=>{
         console.log(data)
